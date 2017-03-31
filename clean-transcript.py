996b607dc3acadad9f60ab32c2b4322b2d4917a8
tshields44 <tshields44@gmail.com>
2017-03-30 19:04:06 -0700

    Add clean transcript files

---
+++ clean-transcript.py
@@ -0,0 +1,17 @@
+import re
+
+
+def clean(text):
+    text = re.sub("\d:\d+:\d+\.\d S(\d+|\?): ", "", text)
+    text = re.sub("\[.+?\]", "", text)
+    text = re.sub("\-", " ", text)
+    text = re.sub("\s{2,}", " ", text)
+    return text.strip()
+
+with open(transcript) as fh:
+    lines = fh.readlines()
+
+lines = [clean(line) for line in lines]
+lines = [line for line in lines if len(line.strip()) > 0]
+
+raw_text = ' '.join(lines)
